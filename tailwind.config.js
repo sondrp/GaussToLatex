08c691f086991e1f226918fb6c3d0d823d4adccd
Sondre Pedersen <sondrp@stud.ntnu.no>
2024-09-05 09:39:24 +0200

    add cool colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'body': ['Calibri', 'ui-serif', 'Georgia'] 
+    },
+    extend: {
+      colors: {
+        ol: {
+          100: "#54daff",
+          200: "#249ee4",
+          300: "#0078d4",
+          400: "#034fa0",
+          500: "#0f3464",
+        }
+      }
+    },
   },
   plugins: [],
 }
