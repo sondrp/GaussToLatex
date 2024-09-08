13a70389f1bf6669e66efbb90b54804665f37bdc
Sondre Pedersen <sondrp@stud.ntnu.no>
2024-09-08 16:07:53 +0200

    implement breakpoints

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useRef, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
 import { cn } from './utils/cn'
 import arrowLeft from './assets/arrowLeft.svg'
 import { useImmer } from 'use-immer'
@@ -6,6 +6,7 @@ import MatrixCreator from './components/MatrixCreator'
 import MatrixDisplay from './components/MatrixDisplay'
 import MatrixEditorTable from './components/MatrixEditorTable'
 import Navbar from './components/Navbar'
+import LaTextarea from './components/LaTextarea'
 
 export const ItemTypes = {
   ROW: 'row',
@@ -15,12 +16,12 @@ export const ItemTypes = {
 function App() {
   // string[][] is temporary data type for matrix. Very likely that I move this to a hook, along with operations for changing the data
   const [matrices, setMatrices] = useImmer<string[][][]>([]) // Hard to stop using Immer for nested objects after having tried once
+  const [breakpoints, setBreakpoints] = useImmer<boolean[]>([])
   const [selectedMatrixIndex, setSelectedMatrixIndex] = useState(0)
 
   const pushMatrix = (matrix: string[][]) => {
-    setMatrices((draft) => {
-      draft.push(matrix)
-    })
+    setMatrices([...matrices, matrix])
+    setBreakpoints([...breakpoints, false])
     setSelectedMatrixIndex(matrices.length)
   }
 
@@ -47,11 +48,20 @@ function App() {
     })
   }
 
+  const toggleBreakpoint = (index: number) => {
+    setBreakpoints((draft) => {
+      draft[index] = !draft[index]
+    })
+  }
+
   const commitAndContinue = () => {
     if (selectedMatrixIndex < matrices.length) {
       const newMatrix = matrices[selectedMatrixIndex].map((row) => [...row])
       setMatrices((draft) => {
         draft.splice(selectedMatrixIndex, 0, newMatrix)
+      })
+      setBreakpoints((draft) => {
+        draft.splice(selectedMatrixIndex, 0, false)
       })
       setSelectedMatrixIndex((old) => old + 1) // increment selection to select the new matrix
     }
@@ -61,84 +71,64 @@ function App() {
     <div className="min-h-screen bg-slate-200">
       <Navbar>
         <MatrixCreator pushMatrix={pushMatrix} />
-        <div className="flex items-center gap-2 transition-all duration-500">
+        <div className="flex w-[600px] flex-wrap items-center gap-2 border-x transition-all duration-500">
           {matrices.map((matrix, i) => (
             <Fragment key={i}>
               <MatrixDisplay
                 deleteMatrix={() => deleteMatrix(i)}
                 className={cn(
+                  'first:ml-8',
                   i === selectedMatrixIndex &&
                     'scale-110 shadow-xl shadow-slate-400',
                 )}
                 onClick={() => setSelectedMatrixIndex(i)}
                 matrix={matrix}
               />
-              <img
-                src={arrowLeft}
-                className="size-6 rotate-180 last:hidden"
-                alt="math arrow next"
-              />
+              {breakpoints[i] && <div className="w-full" />}
+              <button
+                onClick={() => toggleBreakpoint(i)}
+                className="last:hidden"
+              >
+                <img
+                  src={arrowLeft}
+                  className="size-6 rotate-180"
+                  alt="math arrow next"
+                />
+              </button>
             </Fragment>
           ))}
         </div>
       </Navbar>
-      <div className="flex flex-col h-full w-full gap-10 py-20 px-80">
-        {selectedMatrixIndex < matrices.length && (
-          <>
-            <MatrixEditorTable
-              swapRows={swapRows}
-              editCell={editCell}
-              matrix={matrices[selectedMatrixIndex]}
-            />
-            <button
-              className="rounded-md border border-black bg-green-700 px-4 py-2 text-slate-100 w-fit"
-              onClick={commitAndContinue}
-            >
-              Opprett kopi og gå videre
-            </button>
-          </>
-        )}
-        <div className="flex w-full flex-col gap-2">
-          <LaTextArea lines={[amstmath]} />
-          <LaTextArea lines={[beginMatrix, ...generateLatex([]), endMatrix, "",  "% Dummy data for now"]} />
+      <div className="flex size-full justify-between gap-10 py-20 pl-80 pr-20">
+        <div className="flex flex-col gap-2">
+          {selectedMatrixIndex < matrices.length && (
+            <>
+              <MatrixEditorTable
+                swapRows={swapRows}
+                editCell={editCell}
+                matrix={matrices[selectedMatrixIndex]}
+              />
+              <button
+                className="w-fit whitespace-nowrap rounded-md border border-black bg-green-700 px-4 py-2 text-slate-100"
+                onClick={commitAndContinue}
+              >
+                Copy & continue
+              </button>
+            </>
+          )}
+        </div>
+        <div className="flex w-1/3 flex-col items-end gap-2">
+          <textarea
+            readOnly
+            className="h-14 w-80 resize-none bg-slate-300 p-4 outline-none"
+            value="\usepackage{amsmath}"
+          />
+          {matrices.length > 0 && (
+            <LaTextarea breakpoints={breakpoints} matrices={matrices} />
+          )}
         </div>
       </div>
     </div>
-  )
-}
-
-function generateLatex(matrix: string[][]): string[] {
-
-  // dummy data for now
-  return [
-    "1 & 2 & 3 \\\\",
-    "4 & 5 & 6 \\\\",
-    "7 & 8 & 9",
-  ]
-}
-
-const amstmath = '\\usepackage{amsmath}'
-const beginMatrix = `\\[
-\\begin{bmatrix}`
-const endMatrix = `\\end{bmatrix}
-\\]`
-
-function LaTextArea({ lines }: { lines: string[] }) {
-  const ref = useRef<HTMLTextAreaElement>(null)
-
-  useEffect(() => {
-    if (!ref.current) return
-    ref.current.style.height = '40px'
-    ref.current.style.height = `${ref.current.scrollHeight}px`
-  }, [lines])
-
-  return (
-    <textarea
-      ref={ref}
-      readOnly
-      className="h-10 w-1/3 resize-none bg-slate-300 p-4 outline-none"
-      value={lines.join('\n')}
-    />
   )
 }
 
