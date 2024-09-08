627ad5ef2dae6c53e2a9986cfd0db6c2c22ca562
Sondre Pedersen <sondrp@stud.ntnu.no>
2024-09-08 16:07:25 +0200

    add breakpoints to the latex generation

--- src/components/LaTextarea.tsx
+++ src/components/LaTextarea.tsx
@@ -1,59 +1,3 @@
-/* REFERENCE 
-
-
-\\begin{align*}
-&\\begin{bmatrix}
-\\begin{array}{ccc|c}
-1 & 2 & -1 & 3 \\\\
-2 & 3 & 3 & 15 \\\\
-1 & 1 & 2 & 8
-\\end{array}
-\\end{bmatrix}
-\\longrightarrow
-\\begin{bmatrix}
-\\begin{array}{ccc|c}
-1 & 2 & -1 & 3 \\\\
-0 & -1 & 5 & 9 \\\\
-1 & 1 & 2 & 8
-\\end{array}
-\\end{bmatrix}
-\\longrightarrow
-\\begin{bmatrix}
-\\begin{array}{ccc|c}
-1 & 2 & -1 & 3 \\\\
-0 & -1 & 5 & 9 \\\\
-0 & -1 & 3 & 5
-\\end{array}
-\\end{bmatrix} \\\\
-&\\begin{bmatrix}
-\\begin{array}{ccc|c}
-1 & 2 & -1 & 3 \\\\
-2 & 3 & 3 & 15 \\\\
-1 & 1 & 2 & 8
-\\end{array}
-\\end{bmatrix}
-\\longrightarrow
-\\begin{bmatrix}
-\\begin{array}{ccc|c}
-1 & 2 & -1 & 3 \\\\
-0 & -1 & 5 & 9 \\\\
-1 & 1 & 2 & 8
-\\end{array}
-\\end{bmatrix}
-\\longrightarrow
-\\begin{bmatrix}
-\\begin{array}{ccc|c}
-1 & 2 & -1 & 3 \\\\
-0 & -1 & 5 & 9 \\\\
-0 & -1 & 3 & 5
-\\end{array}
-\\end{bmatrix}
-\\end{align*}
-    
-
-
-*/
-
 import { useRef, useEffect } from 'react'
 
 const generateLatexRows = (matrix: string[][]): string => {
@@ -66,13 +10,27 @@ ${matrix.map((row) => row.join(' & ')).join(' \\\\\n')}
 \\end{bmatrix}`
 }
 
-export default function LaTextarea({ matrices }: { matrices: string[][][] }) {
+const matrixRowSeparator = (newline: boolean) =>
+  newline ? '\n\\\\\\longrightarrow&\n' : '\n\\longrightarrow\n'
+
+type Props = {
+  matrices: string[][][]
+  breakpoints: boolean[]
+}
+
+export default function LaTextarea(props: Props) {
+  const { matrices, breakpoints } = props
   const ref = useRef<HTMLTextAreaElement>(null)
 
-  const latex = `\\usepackage{amsmath}
 
-\\begin{align*}
-&${matrices.map((matrix) => generateLatexRows(matrix)).join('\n\\longrightarrow\n')}
+  const latex = `\\begin{align*}
+&${matrices
+    .map((matrix) => generateLatexRows(matrix))
+    .reduce((acc, str, index) => {
+      console.log()
+      if (matrices.length - 1 === index) return acc + str
+      return acc + str + matrixRowSeparator(breakpoints[index])
+    }, '')}
 \\end{align*}`
 
   useEffect(() => {
@@ -85,7 +43,7 @@ export default function LaTextarea({ matrices }: { matrices: string[][][] }) {
     <textarea
       ref={ref}
       readOnly
-      className="h-10 w-1/3 resize-none bg-slate-300 p-4 outline-none"
+      className="h-10 w-80 resize-none bg-slate-300 p-4 outline-none"
       value={latex}
     />
   )
